2e0010af517c0d434b20c79df515ddf8ef97a9d6
Alireza Saadatnia <alirezasn3@gmail.com>
2023-06-05 14:06:24 +0330

    testing kv

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useSignal, $, useVisibleTask$ } from "@builder.io/qwik";
+import { component$, useSignal, $ } from "@builder.io/qwik";
 import {
   DocumentHead,
   // routeAction$,
